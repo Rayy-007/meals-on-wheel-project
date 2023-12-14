ca44045420b24062907b6c5fc2ccb9eda5e0a486
Rayy-007 <minkhant88887@gmail.com>
2023-12-14 09:23:25 +0630

    created pages import and imported

---
+++ src/components/PagesImport.js
@@ -0,0 +1,29 @@
+import Home from "./home/Home";
+import Contact from "./contact/Contact";
+import Login from "./login/Login";
+import Register from "./register/Register";
+// Below pages only can accessable after login
+import Meals from "./users/meals/Meals";
+import Volunteer from "./users/volunteer/Volunteer";
+import Rider from "./users/rider/Rider";
+import Caregiver from "./users/caregiver/Caregiver";
+// Management Pages
+import Admin from "./management/admin/Admin";
+import Delivery from "./management/delivery/Delivery";
+import Feedback from "./management/feedback/Feedback";
+import FoodSafety from "./management/foodSafety/FoodSafety";
+
+export {
+  Home,
+  Contact,
+  Login,
+  Register,
+  Meals,
+  Volunteer,
+  Rider,
+  Caregiver,
+  Admin,
+  Delivery,
+  Feedback,
+  FoodSafety,
+};

0fcd9d93cac5d93f316c1206182116163ef43b7f
Rayy-007 <minkhant88887@gmail.com>
2023-12-14 14:16:49 +0630

    created about first section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,40 @@
 import "./about.css";
+import {
+  HomeDonate,
+  HomeGiveFood,
+  HomeVolunteer,
+  Arrow,
+} from "../ImagesImport";
+import { NavLink } from "react-router-dom";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="about-con">
+      <section className="about-first flex container ">
+        <div className="box">
+          <img src={HomeDonate} alt="donate box" />
+          <h3>We received Donation</h3>
+          <NavLink>
+            Donate <img src={Arrow} alt="arrow" />
+          </NavLink>
+        </div>
+        <div className="box">
+          <img src={HomeVolunteer} alt="donate box" />
+          <h3>We Prepare Meal for people</h3>
+          <NavLink>
+            Join with us <img src={Arrow} alt="arrow" />
+          </NavLink>
+        </div>
+        <div className="box">
+          <img src={HomeGiveFood} alt="donate box" />
+          <h3>We deliver People</h3>
+          <NavLink>
+            Get Meals <img src={Arrow} alt="arrow" />
+          </NavLink>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default About;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,61 +5,66 @@ import {
   HomeBanner1,
   HomeBanner2,
 } from "../ImagesImport";
+import About from "../about/About";
 
 const Home = () => {
   return (
-    <div className="home-container container">
-      <section className="home flex">
-        <div className="left intro">
-          <div className="text">
-            <h1>
-              Delivering <span>Nourishment</span>, Bringing <span>smiles</span>
-            </h1>
-            <p>
-              Welcome to Meals On Wheels, where compassion meets cuisine. We are
-              a dedicated team committed to delivering hot, nutritious meals to
-              qualified adults in need. Our mission is simple — ensuring that no
-              one goes hungry. Whether it's due to age, illness, or disability,
-              we're here to provide a helping hand.
-            </p>
+    <>
+      <div className="home-container container">
+        <section className="home flex">
+          <div className="left intro">
+            <div className="text">
+              <h1>
+                Delivering <span>Nourishment</span>, Bringing{" "}
+                <span>smiles</span>
+              </h1>
+              <p>
+                Welcome to Meals On Wheels, where compassion meets cuisine. We
+                are a dedicated team committed to delivering hot, nutritious
+                meals to qualified adults in need. Our mission is simple —
+                ensuring that no one goes hungry. Whether it's due to age,
+                illness, or disability, we're here to provide a helping hand.
+              </p>
+            </div>
+            <div className="btn-container flex">
+              <a href="/getmeal" className="flex btn primary ">
+                Get Meal <img src={GetMealIcon} alt="get meal icon" />
+              </a>
+              <a href="/donate" className="flex btn violet ">
+                Donate <img src={DonateIcon} alt="donate icon" />
+              </a>
+            </div>
           </div>
-          <div className="btn-container flex">
-            <a href="/getmeal" className="flex btn primary ">
-              Get Meal <img src={GetMealIcon} alt="get meal icon" />
-            </a>
-            <a href="/donate" className="flex btn violet ">
-              Donate <img src={DonateIcon} alt="donate icon" />
-            </a>
+          <div className="right">
+            <img className="first-img" src={HomeBanner1} alt="Home Image" />
+            <img
+              className="second-img"
+              src={HomeBanner2}
+              alt="Home Image Woman"
+            />
           </div>
-        </div>
-        <div className="right">
-          <img className="first-img" src={HomeBanner1} alt="Home Image" />
-          <img
-            className="second-img"
-            src={HomeBanner2}
-            alt="Home Image Woman"
-          />
-        </div>
-      </section>
-      <div className="status">
-        <div>
-          <h3>$20M+</h3>
-          <p>Money Donated</p>
-        </div>
-        <div>
-          <h3>👨‍🦳4583+</h3>
-          <p>Donation Received</p>
-        </div>
-        <div>
-          <h3>😇1032+</h3>
-          <p>Successful Campaign</p>
-        </div>
-        <div>
-          <h3>❣38146+</h3>
-          <p>Volunteers</p>
+        </section>
+        <div className="status">
+          <div>
+            <h3>$20M+</h3>
+            <p>Money Donated</p>
+          </div>
+          <div>
+            <h3>👨‍🦳4583+</h3>
+            <p>Donation Received</p>
+          </div>
+          <div>
+            <h3>😇1032+</h3>
+            <p>Successful Campaign</p>
+          </div>
+          <div>
+            <h3>❣38146+</h3>
+            <p>Volunteers</p>
+          </div>
         </div>
       </div>
-    </div>
+      <About />
+    </>
   );
 };
 

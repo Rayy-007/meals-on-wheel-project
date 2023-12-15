5083ccf621685b794ec697a5b59422e904b90abb
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2023-12-15 11:02:51 +0630

    Creating Contact page.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,29 @@
+import "./contact.css";
+import { HomeBanner2 } from "../ImagesImport";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <div className="contact-container container">
+        <section className="flex contact">
+          <div className="left">
+            
+              <h3>Get In Touch With Us</h3>
+            
+            <div className="input-form">
+              <input type="text" placeholder="Name" className="NameInputBox"/><br/>
+              <input type="text" placeholder="email" className="NameInputBox"/><br/>
+              <textarea type="text" placeholder="Message" className="NameInputBox"/><br/>
+            </div>
+            <button className="btn primary contact-btn">Send</button>
+          </div>
+          <div className="right">
+            <img src={HomeBanner2} alt="Contact Image" />
+          </div>
+        </section> 
+      </div>
+    </>
+  );
 };
 
 export default Contact;

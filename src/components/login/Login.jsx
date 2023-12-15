c2b6136d608ebf3c2082f956924f72e6b1327015
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2023-12-15 14:20:47 +0630

    Creating Contact page and Login page for MerryMeal website.

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,30 @@
+
+import "./login.css"
+import { HomeBanner2 } from "../ImagesImport";
+
 const Login = () => {
-  return <div>Login Changes</div>;
+  return (
+    <>
+      <div className="contact-container container">
+        <section className="flex contact">
+          <div className="left">
+            
+            <h3>Wellcome Back !</h3>
+            <p>Login !</p>
+            
+            <div className="input-form">
+              <input type="text" placeholder="Email" className="NameInputBox"/><br/>
+              <input type="text" placeholder="Password" className="NameInputBox"/><br/>
+            </div>
+            <button className="btn primary login-btn">Login</button>
+          </div>
+          <div className="right">
+            <img src={HomeBanner2} alt="Contact Image" />
+          </div>
+        </section> 
+      </div>
+    </>
+  );
 };
 
 export default Login;

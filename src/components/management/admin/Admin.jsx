2907d425716aae1c6a52595f4acc2377bd3ce96a
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2023-12-18 10:32:28 +0630

    Adding Admin Pages.

--- src/components/management/admin/Admin.jsx
+++ src/components/management/admin/Admin.jsx
@@ -1,5 +1,29 @@
-const Admin = () => {
-  return <div>Admin</div>;
-};
+import React from 'react'
+import "./admin.css"
+import { Link, Outlet } from 'react-router-dom'
 
-export default Admin;
+export default function () {
+
+  return (
+    <div className='container'>
+      <div className='title'>
+        <h2>Admin Dashboard</h2>
+        <p style={{fontWeight:500}}>All Member, Care Givers, Volunteer, Partner, Rider And Donator Data Are Shown Below.</p>
+      </div>
+      <div className='flex-btn'>
+        <nav>
+          <Link to='member'><button className='memeber button'>Members</button></Link>
+          <Link to='caregiver'><button className='memeber button'>Care Givers</button></Link>
+          <Link to='volunteer'><button className='memeber button'>Volunteers</button></Link>
+          <Link to='partner'><button className='memeber button'>Partners</button></Link>
+          <Link to='donator'><button className='memeber button'>Dontators</button></Link>
+          <Link to='rider'><button className='memeber button'>Riders</button></Link>
+        </nav>
+      </div>
+      
+        <Outlet />
+      
+    </div>
+  )
+}
+
--- src/components/management/delivery/Delivery.jsx
+++ src/components/management/delivery/Delivery.jsx
@@ -2,78 +2,179 @@ import "../delivery/delivery.css";
 
 const Delivery = () => {
   const data = [
-    { deliID: "001", memberName: "Kyaw Kyaw", memberAdresss: "87*88 100Street Mandalay", deliveryTime: "10:00AM", riderName: "Oo Ko", riderID: "007"  },
-    { deliID: "001", memberName: "Kyaw Kyaw", memberAdresss: "87*88 100Street Mandalay", deliveryTime: "10:00AM", riderName: "Oo Ko", riderID: "007"  },
-    { deliID: "001", memberName: "Kyaw Kyaw", memberAdresss: "87*88 100Street Mandalay", deliveryTime: "10:00AM", riderName: "Oo Ko", riderID: "007"  },
-    { deliID: "001", memberName: "Kyaw Kyaw", memberAdresss: "87*88 100Street Mandalay", deliveryTime: "10:00AM", riderName: "Oo Ko", riderID: "007"  },
-    { deliID: "001", memberName: "Kyaw Kyaw", memberAdresss: "87*88 100Street Mandalay", deliveryTime: "10:00AM", riderName: "Oo Ko", riderID: "007"  },
-    { deliID: "001", memberName: "Kyaw Kyaw", memberAdresss: "87*88 100Street Mandalay", deliveryTime: "10:00AM", riderName: "Oo Ko", riderID: "007"  },
-  ]
+    {
+      deliID: "001",
+      memberName: "Kyaw Kyaw",
+      memberAdresss: "87*88 100Street Mandalay",
+      deliveryTime: "10:00AM",
+      riderName: "Oo Ko",
+      riderID: "007",
+    },
+    {
+      deliID: "001",
+      memberName: "Kyaw Kyaw",
+      memberAdresss: "87*88 100Street Mandalay",
+      deliveryTime: "10:00AM",
+      riderName: "Oo Ko",
+      riderID: "007",
+    },
+    {
+      deliID: "001",
+      memberName: "Kyaw Kyaw",
+      memberAdresss: "87*88 100Street Mandalay",
+      deliveryTime: "10:00AM",
+      riderName: "Oo Ko",
+      riderID: "007",
+    },
+    {
+      deliID: "001",
+      memberName: "Kyaw Kyaw",
+      memberAdresss: "87*88 100Street Mandalay",
+      deliveryTime: "10:00AM",
+      riderName: "Oo Ko",
+      riderID: "007",
+    },
+    {
+      deliID: "001",
+      memberName: "Kyaw Kyaw",
+      memberAdresss: "87*88 100Street Mandalay",
+      deliveryTime: "10:00AM",
+      riderName: "Oo Ko",
+      riderID: "007",
+    },
+    {
+      deliID: "001",
+      memberName: "Kyaw Kyaw",
+      memberAdresss: "87*88 100Street Mandalay",
+      deliveryTime: "10:00AM",
+      riderName: "Oo Ko",
+      riderID: "007",
+    },
+  ];
   return (
-    
-  <div className="container">
+    <div className="container">
+      <div className="">
+        <h3 style={{ color: "#f8590af6" }}>Assign Delivery Information </h3>
+        <br />
 
-<div className="">
-        <h3 style={{color: "#f8590af6"}}>Assign Delivery Information </h3><br/>
-        
         <table>
-        <thead>
+          <thead>
+            <tr>
+              <th>Member ID</th>
+              <th>Member Name</th>
+              <th>Member Address</th>
+              <th>Delivery Time</th>
+              <th>Rider Name</th>
+              <th>Rider ID</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <input
+                  type="text"
+                  name="memberID"
+                  placeholder="Member ID.."
+                  required
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="memberName"
+                  placeholder="Member name.."
+                  required
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="address"
+                  placeholder="Enter Member Address.."
+                  required
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="deliveryTime"
+                  placeholder="Delivery Time.."
+                  required
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="riderName"
+                  placeholder="Rider Name.."
+                  required
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="riderID"
+                  placeholder="Rider ID.."
+                  required
+                />
+              </td>
+              <td>
+                <input
+                  type="submit"
+                  className="btn green btn-custom"
+                  value="Submit"
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <br />
+      <div className="">
+        <h3 style={{ color: "#f8590af6" }}>Scheduled lists to Delivery</h3>
+        <br />
+
+        <table className="table-schedule">
           <tr>
-          <th>Member ID</th>
+            <th>Delivery ID</th>
             <th>Member Name</th>
             <th>Member Address</th>
             <th>Delivery Time</th>
             <th>Rider Name</th>
             <th>Rider ID</th>
-            <th>Action</th>
+            <th></th>
+            <th></th>
           </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td><input  type="text" name="memberID" placeholder="Member ID.." required /></td>
-            <td><input type="text" name="memberName" placeholder="Member name.." required /></td>
-            <td><input type="text" name="address" placeholder="Enter Member Address.." required /></td>
-            <td><input type="text" name="deliveryTime" placeholder="Delivery Time.." required /></td>
-            <td><input type="text" name="riderName" placeholder="Rider Name.." required /></td>
-            <td><input type="text" name="riderID" placeholder="Rider ID.." required /></td>
-            <td><input type="submit" className="btn green btn-custom" value="Submit" /></td>
-          </tr>
-        </tbody>
-      </table>
-
+          {data.map((val, key) => {
+            return (
+              <tr key={key}>
+                <td>{val.deliID}</td>
+                <td>{val.memberName}</td>
+                <td>{val.memberAdresss}</td>
+                <td>{val.deliveryTime}</td>
+                <td>{val.riderName}</td>
+                <td>{val.riderID}</td>
+                <td>
+                  <input
+                    type="submit"
+                    className="btn primary btn-custom"
+                    value="Edit"
+                  />
+                </td>
+                <td>
+                  <input
+                    type="submit"
+                    className="btn red btn-custom"
+                    value="Delete"
+                  />
+                </td>
+              </tr>
+            );
+          })}
+        </table>
       </div>
-      <br/>
-          <div className="">
-        <h3 style={{color: "#f8590af6"}}>Scheduled lists to Delivery</h3><br/>
-        
-            <table className="table-schedule">
-                <tr>
-                    <th>Delivery ID</th>
-                    <th >Member Name</th>
-                    <th >Member Address</th>
-                    <th >Delivery Time</th>
-                    <th >Rider Name</th>
-                    <th >Rider ID</th>
-                </tr>
-                {data.map((val, key) => {
-                    return (
-                        <tr key={key}>
-                            <td >{val.deliID}</td>
-                            <td>{val.memberName}</td>
-                            <td >{val.memberAdresss}</td>
-                            <td >{val.deliveryTime}</td>
-                            <td >{val.riderName}</td>
-                            <td >{val.riderID}</td>
-                            <td ><input type="submit" className="btn primary btn-custom" value="Edit" /></td>
-                            <td ><input type="submit" className="btn red btn-custom" value="Delete" /></td>
-                        </tr>
-                    )
-                })}
-            </table>
-        
-      </div>
-    
-  </div>
+    </div>
   );
 };
 

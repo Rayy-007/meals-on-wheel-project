d6df1982c4fbc460ced63cdb896dadbff70eb333
Rayy-007 <minkhant88887@gmail.com>
2024-01-02 13:55:25 +0630

    wrong click

--- src/components/users/meals/Meals.jsx
+++ src/components/users/meals/Meals.jsx
@@ -14,46 +14,46 @@ import { useState, useEffect } from "react";
 import { useAuth } from "../../login/AuthContext";
 import MealService from "../../management/admin/service/MealService";
 
-
 const Meals = () => {
-
   const [meals, setMeals] = useState([]);
   const { user } = useAuth();
   const [userLocation, setUserLocation] = useState(null);
 
-    useEffect(() => {
-      MealService.getAllMeals()
-        .then(res => setMeals(res.data))
-        .catch(error => console.error('Error fetching meals:', error));
-    }, []); // Empty dependency array to run the effect only once on mount
+  useEffect(() => {
+    MealService.getAllMeals()
+      .then((res) => setMeals(res.data))
+      .catch((error) => console.error("Error fetching meals:", error));
+  }, []); // Empty dependency array to run the effect only once on mount
 
-      const handleOrderClick = (mealId) => {
-        const memberId = user?.memberId;
-    
-        if (!memberId) {
-          console.error('Member ID not found.');
-          return;
-        }
-    
-        const orderData = {
-            memberId: memberId,
-            mealId: mealId,
-          };
-  
-      // Make a POST request to create an order
-      axios.post(`http://localhost:8080/order-api/${memberId}/order/${mealId}`, orderData)
-        .then((res) => {
-          console.log("Order created:", res.data);
-          // Optionally, you can update the state or show a success message to the user
-        })
-        .catch((error) => {
-          console.error("Error creating order:", error);
-          // Handle the error (e.g., show an error message to the user)
-        });
-  
+  const handleOrderClick = (mealId) => {
+    const memberId = user?.memberId;
+
+    if (!memberId) {
+      console.error("Member ID not found.");
+      return;
+    }
+
+    const orderData = {
+      memberId: memberId,
+      mealId: mealId,
+    };
+
+    // Make a POST request to create an order
+    axios
+      .post(
+        `http://localhost:8080/order-api/${memberId}/order/${mealId}`,
+        orderData
+      )
+      .then((res) => {
+        console.log("Order created:", res.data);
+        // Optionally, you can update the state or show a success message to the user
+      })
+      .catch((error) => {
+        console.error("Error creating order:", error);
+        // Handle the error (e.g., show an error message to the user)
+      });
   };
 
-  
   const handleUserLocationChange = (userLocation) => {
     setUserLocation(userLocation);
     console.log(userLocation);
@@ -67,7 +67,7 @@ const Meals = () => {
     const R = 6371; // Earth radius in kilometers
     const lat1 = userLocation.lat();
     const lon1 = userLocation.lng();
-    const [latStr, lonStr] = partnerLocation.split(','); // Assuming the delimiter is a comma
+    const [latStr, lonStr] = partnerLocation.split(","); // Assuming the delimiter is a comma
     const lat2 = parseFloat(latStr);
     const lon2 = parseFloat(lonStr);
 
@@ -87,29 +87,36 @@ const Meals = () => {
     return distance;
   };
 
-    
   return (
     <div className="container-fluid">
       <MapComponent onUserLocationChange={handleUserLocationChange} />
-    <div className="row">
-      <div className="card-group">
-        {meals.map(meal => (
-          <div className="card" key={meal.id}>
-            <img className="mealphoto" src={`http://localhost:8080/uploads/images/${meal.mealPhoto}`} alt={meal.mealName} />
-            <div className="card-body">
-              <h5 className="card-title">Meal Name : {meal.mealName} </h5>
-              <p className="card-text">Meal Desc : {meal.mealDesc} </p>
-              <p className="card-text">Partner Name : {meal.partner.companyName} </p>
-              <p className="card-text">
+      <div className="row">
+        <div className="card-group">
+          {meals.map((meal) => (
+            <div className="card" key={meal.id}>
+              <img
+                className="mealphoto"
+                src={`http://localhost:8080/uploads/images/${meal.mealPhoto}`}
+                alt={meal.mealName}
+              />
+              <div className="card-body">
+                <h5 className="card-title">Meal Name : {meal.mealName} </h5>
+                <p className="card-text">Meal Desc : {meal.mealDesc} </p>
+                <p className="card-text">
+                  Partner Name : {meal.partner.companyName}{" "}
+                </p>
+                <p className="card-text">
                   Distance: {calculateDistance(meal.partner.companyLocation)} km
-              </p>
-              <button type="button" onClick={() => handleOrderClick(meal.id)}>Order Now</button>
+                </p>
+                <button type="button" onClick={() => handleOrderClick(meal.id)}>
+                  Order Now
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
-  </div>
   );
 };
 
